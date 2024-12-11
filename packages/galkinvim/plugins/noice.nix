1249aa7c3d68b4f2075ef16323229dba04b4ba2a
Mykola Halkin <mykola.halkin@nixs.com>
2024-12-11 14:46:34 +0100

    fix(noice)

--- packages/galkinvim/plugins/noice.nix
+++ packages/galkinvim/plugins/noice.nix
@@ -2,13 +2,14 @@
 {
   plugins.noice = {
     enable = true;
-    lsp = {
-      override = {
-        "vim.lsp.util.convert_input_to_markdown_lines" = true;
-        "vim.lsp.util.stylize_markdown" = true;
-        "cmp.entry.get_documentation" = true;
+    settings = {
+      lsp = {
+        override = {
+          "vim.lsp.util.convert_input_to_markdown_lines" = true;
+          "vim.lsp.util.stylize_markdown" = true;
+          "cmp.entry.get_documentation" = true;
+        };
       };
     };
   };
-
 }
